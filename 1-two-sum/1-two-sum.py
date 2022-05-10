8aaa9acd49665cc3088b7426e9f2d0ec20ff239e
alexis <103905871+awillis-dev@users.noreply.github.com>
2022-05-10 03:12:47 -0700

    Time: 67 ms (82.80%), Space: 15.1 MB (49.77%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        prev = {} #val: idx
+        
+        for i in range(len(nums)):
+            
+            diff = target - nums[i]
+            if diff in prev:
+                return (prev[diff], i)
+            prev[nums[i]] = i
+            
