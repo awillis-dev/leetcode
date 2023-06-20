b99a69fa4eb2d82de76f6e510229699670ed1bf8
alexis <103905871+awillis-dev@users.noreply.github.com>
2023-06-19 20:15:20 -0700

    Time: 61 ms (85.52%), Space: 44.2 MB (45.04%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -24,15 +24,15 @@ var mergeTwoLists = function(list1, list2) {
             list2 = list2.next
         }
         curr = curr.next
-    
     }
     
-     if (list1 !== null) {
-            curr.next = list1
-     }
+    if (list1 !== null) {
+        curr.next = list1
+    }
+    
     if (list2 !== null) {
-            curr.next = list2
-        }
+        curr.next = list2
+    }
     
     return node.next
 };
