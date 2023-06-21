032b35ef8e0d56045e5eb17ad2166ec49da079ba
alexis <103905871+awillis-dev@users.noreply.github.com>
2023-06-21 14:49:54 -0700

    Time: 64 ms (58.13%), Space: 42.1 MB (78.24%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -4,31 +4,34 @@
  */
 var isValid = function(s) {
     
-    // check length of s, return false if odd
-    if(s.length % 2 !== 0) {
+            
+    // push open () to stack
+    // if curr is closed() & stack isnt empty & last stack item is matching opening(), pop
+    // return true if stack is empty
+    
+    if (s.length % 2 !== 0) {
         return false
     }
-    
-    // create stack var
+        
+        
     let stack = []
     
-    // loop through string
-        // push all opening brackets onto new stack
-        // check all closing bracket cases: if char is a closing bracket, stack isnt empty, and stack contains corresponding opening bracket, pop. else: return false. return true if stack length = 0
-        for(let char of s) {
+    for (char of s) {
         
-        if(char === '(' || char === '[' || char === '{') {
+        if (char === '(' || char === '[' || char === '{') {
             stack.push(char)
-        }
+        } 
         
-        else if ( (char === ')' && stack.length !== 0 && stack[stack.length-1] === '(') || 
-            (char === ']' && stack.length !== 0 && stack[stack.length-1] === '[') ||
-            (char === '}' && stack.length !== 0 && stack[stack.length-1] === '{') ) {
-            stack.pop()
+        else if ((char === ')' && stack.length !== 0 && stack[stack.length-1] === '(') ||
+                (char === ']' && stack.length !== 0 && stack[stack.length-1] === '[') ||
+                (char === '}' && stack.length !== 0 && stack[stack.length-1] === '{')) {
+                stack.pop()           
         } else {
             return false
         }
+        
     }
     
     return stack.length === 0
+
 };
